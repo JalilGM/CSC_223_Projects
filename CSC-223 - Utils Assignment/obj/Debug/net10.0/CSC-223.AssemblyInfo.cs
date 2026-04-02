9e4d599c196fe5e5ddc05aa473d64e230f3808a0
JalilGM <garvja4@furman.edu>
2026-04-02 09:01:01 -0400

    added the different cs files for the assignment

--- CSC-223 - Utils Assignment/obj/Debug/net10.0/CSC-223.AssemblyInfo.cs
+++ CSC-223 - Utils Assignment/obj/Debug/net10.0/CSC-223.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSC-223")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a96fca197e37f58c80eb22e1ca41e2a79508ab0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f1b55f6bc26e7365515e8e417b0332fd3525a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSC-223")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSC-223")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
