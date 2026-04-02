6e821a1178f2de8fe13d9db71be1021e7a534362
JalilGM <garvja4@furman.edu>
2026-04-02 14:24:39 -0400

    finished implementing in EvaluateVisitor

--- CSC-223 - Utils Assignment/src/AST/Visitors/EvaluateVisitor.cs
+++ CSC-223 - Utils Assignment/src/AST/Visitors/EvaluateVisitor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using AST;
+using Containers;
 
 namespace AST
 {
@@ -53,11 +54,79 @@ namespace AST
             return _returnValue;
         }
 
-        // TODO
+        private object GetVariableValue(string name, SymbolTable<string, object> symbolTable)
+        {
+            if (symbolTable == null)
+            {
+                throw new EvaluationException($"Variable '{name}' is not defined.");
+            }
+
+            if (symbolTable.TryGetValue(name, out object value))
+            {
+                return value;
+            }
+            else
+            {
+                throw new EvaluationException($"Variable '{name}' is not defined.");
+            }
+        }
 
         #region Expression Node Visit Methods
 
-        // TODO
+        public object Visit(PlusNode node, SymbolTable<string, object> symbolTable)
+        {
+            object leftValue = node.Left.Accept(this, symbolTable);
+            object rightValue = node.Right.Accept(this, symbolTable);
+            return Convert.ToDouble(leftValue) + Convert.ToDouble(rightValue);
+
+        }
+
+        public object Visit(MinusNode node, SymbolTable<string, object> symbolTable)
+        {
+            object leftValue = node.Left.Accept(this, symbolTable);
+            object rightValue = node.Right.Accept(this, symbolTable);
+            return Convert.ToDouble(leftValue) - Convert.ToDouble(rightValue);
+        }
+
+        public object Visit(TimesNode node, SymbolTable<string, object> symbolTable)
+        {
+            object leftValue = node.Left.Accept(this, symbolTable);
+            object rightValue = node.Right.Accept(this, symbolTable);
+            return Convert.ToDouble(leftValue) * Convert.ToDouble(rightValue);
+        }
+
+        public object Visit(FloatDivNode node, SymbolTable<string, object> symbolTable)
+        {
+            object leftValue = node.Left.Accept(this, symbolTable);
+            object rightValue = node.Right.Accept(this, symbolTable);
+            return Convert.ToDouble(leftValue) / Convert.ToDouble(rightValue);
+        }
+
+        public object Visit(IntDivNode node, SymbolTable<string, object> symbolTable)
+        {
+            object leftValue = node.Left.Accept(this, symbolTable);
+            object rightValue = node.Right.Accept(this, symbolTable);
+            return Convert.ToInt32(leftValue) / Convert.ToInt32(rightValue);
+        }
+
+        public object Visit(ModulusNode node, SymbolTable<string, object> symbolTable)
+        {
+            object leftValue = node.Left.Accept(this, symbolTable);
+            object rightValue = node.Right.Accept(this, symbolTable);
+            return Convert.ToInt32(leftValue) % Convert.ToInt32(rightValue);
+        }
+
+        public object Visit(ExponentiationNode node, SymbolTable<string, object> symbolTable)
+        {
+            object leftValue = node.Left.Accept(this, symbolTable);
+            object rightValue = node.Right.Accept(this, symbolTable);
+            return Math.Pow(Convert.ToDouble(leftValue), Convert.ToDouble(rightValue));
+        }
+
+        public object Visit(LiteralNode node, SymbolTable<string, object> symbolTable)
+        {
+            return node.Value;
+        }
 
         public object Visit(VariableNode node, SymbolTable<string, object> symbolTable)
         {
@@ -69,14 +138,43 @@ namespace AST
 
         #region Statement Node Visit Methods
 
-        // TODO
+        public object Visit(AssignmentStmt node, SymbolTable<string, object> symbolTable)
+        {
+            // Evaluate the expression to get the value to assign
+            object value = node.Expression.Accept(this, symbolTable);
+            
+            // Assign the value to the variable in the current symbol table
+            symbolTable[node.Variable.Name] = value;
+            
+            return null; // Assignment statements do not return a value
+        }
+
+        public object Visit(ReturnStmt node, SymbolTable<string, object> symbolTable)
+        {
+            // Evaluate the expression to get the return value
+            _returnValue = node.Expression.Accept(this, symbolTable);
+            _returnEncountered = true;
+            return null; // Return statements do not return a value directly
+        }
 
         public object Visit(BlockStmt node, SymbolTable<string, object> symbolTable)
         {
             // Use this block's symbol table, which is already linked to its parent
             SymbolTable<string, object> currentScope = node.SymbolTable;
             
-            // TODO
+            // Execute each statement in the block
+            foreach (Statement stmt in node.Statements)
+            {
+                stmt.Accept(this, currentScope);
+                
+                // If a return statement was encountered, stop executing further statements
+                if (_returnEncountered)
+                {
+                    break;
+                }
+            }
+
+            return null; // Block statements do not return a value directly
         }
 
         #endregion
